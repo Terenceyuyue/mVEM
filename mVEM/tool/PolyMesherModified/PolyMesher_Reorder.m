ef02ec9ec9b61efbdec4d52e0f864ad275138d6f
terenceyuyue <terenceyuyue@sjtu.edu.cn>
2020-11-13 21:25:59 +0800

    delete all

--- mVEM/tool/PolyMesherModified/PolyMesher_Reorder.m
+++
@@ -1,7 +0,0 @@
-function [node,elem] = PolyMesher_Reorder(NT,node,elem)
-% Extract node list
-elem = elem(1:NT)';
-[id,~,totalid] = unique(horzcat(elem{:})');
-node = node(id,:);
-elemLen = cellfun('length',elem);
-elem = mat2cell(totalid', 1, elemLen)';
