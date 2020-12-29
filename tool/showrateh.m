808436aae59c94b9ca90d51f6538fef3c5b0db29
Terenceyuyue <terenceyuyue@sjtu.edu.cn>
2020-12-29 12:51:43 +0800

    Update showrateh.m

--- tool/showrateh.m
+++ tool/showrateh.m
@@ -1,38 +1,29 @@
 function showrateh(h,varargin)
-%showrateh displays convergence rates of an err sequence
-%
-%  e.g. showrateh(h,ErrL2,ErrH1,ErrH2,ErrI)
-%
+%showrateh displays convergence rates of an err sequence in the form of
+% e.g. 
+%  showrateh(h,ErruL2, 'r-*', '||u-u_h||')
+%  showrateh(h,ErruL2,'r-*','||u-u_h||',  ErrpL2, 'b-s','||p-p_h||')
+% 
 % Copyright (C) Terence Yu.
 
-%% Determine number of Err
-n = 0;
-for i = 2:nargin
-    varName = lower(inputname(i));
-    if mycontains(varName, 'err'),  n = n + 1; end
+optRate = {'k.-', 'k--', 'k-', 'k:.'};
+
+%% Get Err, opt, strErr
+n = (nargin-1)/3; % number of Err
+Err = cell(1,n); opt = cell(1,n); strErr = cell(1,n);
+s = 1;
+for i = 1:n
+    Err{i} = varargin{s};
+    opt{i} = varargin{s+1};
+    strErr{i} = varargin{s+2};
+    s = s+3;
 end
 
 %% Plot convergence rates
-str = cell(1,2*n);  
+str = cell(1,2*n);
 for i = 1:n
-    var = upper(inputname(i+1));
-    if mycontains(var, 'L2') 
-        stri = '||u-u_h||';  
-        r = showrate(h,varargin{i},'r-*','k.-');
-    end
-    if mycontains(var, 'H1')
-        stri = '|u-h_h|_1';  
-        r = showrate(h,varargin{i},'b-s','k--');
-    end
-    if mycontains(var, 'H2')
-        stri = '|u-u_h|_2'; 
-        r = showrate(h,varargin{i},'b-o','r.-');
-    end
-    if mycontains(var, 'I')
-        stri = '||u_I-u_h||_E'; 
-        r = showrate(h,varargin{i},'k-d','k:.');        
-    end
-    str{2*i-1} = stri;
+    r = showrate(h,Err{i},opt{i},optRate{i});
+    str{2*i-1} = strErr{i};
     str{2*i} = ['O (h^{' num2str(r,'%0.2f') '})'];
 end
 
