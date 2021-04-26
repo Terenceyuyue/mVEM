4ea742ad75f822949a0814d828451ffe61531299
Terenceyuyue <terenceyuyue@sjtu.edu.cn>
2021-04-26 11:37:14 +0800

    Update test_PolyMeshRefineTime.m

--- examples/main_mesh/test_PolyMeshRefineTime.m
+++ examples/main_mesh/test_PolyMeshRefineTime.m
@@ -9,7 +9,7 @@ Iter = 8;
 timetable = zeros(Iter, 2);
 nElem = zeros(Iter, 1);
 
-for i = 1 : Iter    
+for i = 1:Iter
     % randomly give the marked elements
     marked = randi(length(elem), round([0.8*length(elem), 1]));
     
@@ -21,21 +21,19 @@ for i = 1 : Iter
     [newnode2, newelem2] = PolyMeshRefine1(node, elem, marked);
     timetable(i,2) = toc;
     
-    % display time
-    disp('Timetable');
-    disp('PolyMeshRefine   PolyMeshRefine1');
-    disp(timetable(1:i,:));
-    
     % update node, elem
     node = newnode2;  elem = newelem2;
-    nElem(i) = size(elem,1);    
+    nElem(i) = size(elem,1);
     
-    % display refined mesh given by PolyMeshRefine
+    % display time
+    disp('Timetable:');
+    colname = {'#NT',' PolyMeshRefine ',' PolyMeshRefine1 '};
+    disptable(colname,nElem(1:i),[],timetable(1:i,1),'%0.5f',timetable(1:i,2),'%0.5f');
+    
+    % display refined mesh given by PolyMeshRefine and PolyMeshRefine1
     subplot(1,2,1);
     showmesh(newnode1, newelem1, opt_mesh);
     axis off;
-    
-    % display refined mesh given by PolyMeshRefine1
     subplot(1,2,2);
     showmesh(newnode2, newelem2, opt_mesh);
     axis off;
